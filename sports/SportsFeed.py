ba8a91ca09f130ea81645d783af0a1a217b3bd8c
Alex <alexjob222@gmail.com>
2017-04-03 23:34:00 -0500

    Conference standings method

--- sports/SportsFeed.py
+++ sports/SportsFeed.py
@@ -30,6 +30,7 @@ class SportsFeed(object):
 		self.league = league
 		self.lastGameUpdate = None
 		self.todaysGames = None
+		self.conferenceStandings = None
 
 	def get_daily_games(self, date):
 		url = '{0}{1}/current/daily_game_schedule.json?fordate={2}'.format(
@@ -55,13 +56,67 @@ class SportsFeed(object):
 					gameList.append(gameObj)
 					
 		except Exception as e:
-			print(e)
+			print(datetime.datetime.now().strftime('%c') + ' - daily games - ' + str(e))
 		
 		self.lastGameUpdate = datetime.datetime.now()
 		self.todaysGames = gameList
 		
 		#Return the list of games
 		return gameList
+		
+	
+	def get_conference_standings(self):
+		#The list of team stats desired in the response
+		stats = 'W,L,'
+		if self.league == 'NHL':
+			stats += 'Pts,OTL'
+		elif self.league == 'NFL':
+			stats += 'T'
+		else:
+			stats += 'GB'
+			
+		url = '{0}{1}/current/conference_team_standings.json?teamstats={2}'.format(
+				self.BASE_URL, self.league, stats)
+		
+		conferenceList = list()
+		
+		try:
+			response = requests.get(url, auth=(config.sports['username'], config.sports['password']))
+			
+			if response.text:
+				#Parse the json into objects
+				data = json.loads(response.text)
+				for conf in data['conferenceteamstandings']['conference']:
+					confName = conf['@name']
+					
+					teamList = list()
+					
+					for team in conf['teamentry']:
+						teamAbbr = team['team']['Abbreviation']
+						rank = team['rank']
+						wins = team['stats']['Wins']['#text']
+						losses = team['stats']['Losses']['#text']
+						extraStats = {}
+						
+						if self.league == 'NHL':
+							extraStats['OTL'] = team['stats']['OvertimeLosses']['#text']
+							extraStats['PTS'] = team['stats']['Points']['#text']
+						elif self.league == 'NFL':
+							extraStats['Ties'] = team['stats']['Ties']['#text']
+						else:
+							extraStats['GB'] = team['stats']['GB']['#text']
+							
+						teamInfo = TeamStandingInfo(self.league, teamAbbr, rank, wins, losses, extraStats)
+						teamList.append(teamInfo)
+						
+					conference = Conference(confName, teamList)
+					conferenceList.append(conference)
+											
+		except Exception as e:
+			print(datetime.datetime.now().strftime('%c') + ' - standings - ' + str(e))
+			
+		self.conferenceStandings = conferenceList
+		return conferenceList
 		
 
 	def _localize_time(self, time):
@@ -95,5 +150,21 @@ class SportsGame(object):
 		
 		self.startTime = startTime
 		self.league = league
+		
+		
+class TeamStandingInfo(object):
+	def __init__(self, league, teamAbbr, rank, wins, losses, extraStats):
+		self.teamAbbr = get_team_abbr(teamAbbr, league)
+		self.teamImgPath = 'images/' + league + '/' + self.team + '.png'
+		self.rank = rank
+		self.wins = wins
+		self.losses = losses
+		self.extraStats = extraStats
+		
+
+class Conference(object):
+	def __init__(self, name, teams):
+		self.name = name
+		self.teams = teams
 		
 	
